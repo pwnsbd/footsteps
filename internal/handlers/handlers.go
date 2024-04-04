d0cbd1398177b987e40b6398de74f33bf51e9f38
pwnsbd <justpawan7@gmail.com>
2024-04-03 23:27:57 -0700

    made the changes

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,15 +2,18 @@ package handlers
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to my page")
+	homeTemplate := template.Must(template.ParseFiles("src/templates/home.html"))
+	homeTemplate.Execute(w, nil)
+
 }
 
 func ContactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "This is the contact page")
+	fmt.Fprint(w, "This is contact page")
 }
 
 func ProjectHandler(w http.ResponseWriter, r *http.Request) {
